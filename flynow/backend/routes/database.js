7d24e14d25391d1625bb4a66e41d0e86ed99fdd2
Armando Benavidez <ajrbenavidez@gmail.com>
2019-10-28 23:10:17 -0700

    working 3 tier on ab machine

--- flynow/backend/routes/database.js
+++ flynow/backend/routes/database.js
@@ -5,7 +5,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'password',
+  password: 'newpassword',
   database: 'cs157a'
 });
 router.get('/', function(req, res, next){
