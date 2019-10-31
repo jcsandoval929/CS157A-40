291a9edbd649b5af5b58ef946561e26ba9ba6cf2
nithil.sethupathi <nithillsethupathi@gmail.com>
2019-10-30 20:57:16 -0700

    app commit

--- flynow/src/App.js
+++ flynow/src/App.js
@@ -6,22 +6,24 @@ function App(){
   // }
   const callIndex = () => {
     fetch("http://localhost:8000/database")
-        .then(res => res.text())
-        .then(res => setApiResponse(res))
-        .catch(err => err);
+    .then(res => res.text())
+    .then(res => setApiResponse(res))
+    .catch(err => err);
   }
   useEffect(() => callIndex(),[]);
   // const UNSAFE_componentWillMount = () => {
   //   this.callIndex();
   // }
   return (
-      <div className = "App">
-          <header className = "Header">
-              <h1 className = "Title">Welcome to FlyNow</h1>
-          </header>
-          <p className = "Intro">{apiResponse}</p>
-      </div>
-    );
+    <div className = "App">
+    <header className = "Header">
+    <h1 className = "Title">Welcome to FlyNow</h1>
+    {' '}
+    <h4 className = "DatabaseTitle"> MySQL Database</h4>
+    </header>
+    <p className = "Database">{apiResponse}</p>
+    </div>
+  );
 }
 
 export default App;
