bd421f22b5de7f00e6b26cc8820aadffe24de587
Juan Carlos Sandoval <jcsandoval929@gmail.com>
2019-10-31 14:43:04 -0700

    Added home page

--- flynow/src/App.js
+++ flynow/src/App.js
@@ -1,23 +1,18 @@
-import React, {Component} from 'react';
-import {BrowserRouter as Router } from 'react-router-dom'
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom'
 import BaseRouter from "./routes"
 
-class App extends Component{
+class App extends Component {
 
-  render(){
-  return(
-  
-      <div>
-        <Router>
-          
-          <BaseRouter/>
-          
-        </Router>
-       
-      </div>
-      ); 
-  }
-  
+    render() {
+        return (
+            <div>
+                <Router>
+                    <BaseRouter />
+                </Router>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ flynow/src/components/home.js
@@ -0,0 +1,49 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+import CardActions from "@material-ui/core/CardActions";
+import Paper from '@material-ui/core/Paper';
+import { Link } from "react-router-dom";
+import "typeface-roboto";
+
+const headerStyle = {
+    color: "#072F5F",
+    fontSize: "100px",
+    fontWeight: "normal",
+    fontFamily: "roboto"
+};
+const buttonStyle = {
+    //marginTop: "10px",
+    fontSize: "20px",
+    padding: "20px",
+    fontWeight: "normal",
+    //marginBottom: "40px"
+};
+
+class home extends React.Component {
+    render() {
+
+        return (
+            <div>
+                <h1 align="center" style={headerStyle}>
+                    FlyNow
+                    </h1>
+                <Paper>
+
+                </Paper>
+                <CardActions style={{ justifyContent: "center" }}>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        component={Link}
+                        to="/login/"
+                        style={buttonStyle}
+                    >
+                        Login/Register
+                    </Button>
+                </CardActions>
+            </div >
+        );
+    }
+}
+
+export default home;
