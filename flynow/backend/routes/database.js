74ede16ababa0598826b92224db3e4b3aca0fe1a
Juan Carlos Sandoval <jcsandoval929@gmail.com>
2019-12-05 04:07:32 -0800

    Search done

--- flynow/backend/routes/database.js
+++ flynow/backend/routes/database.js
@@ -1,148 +1,197 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var mysql = require('mysql');
+var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'newpassword',
-  database: 'flynow'
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "flynow"
 });
 
-connection.connect(function(err){
-if(!err) {
+connection.connect(function(err) {
+  if (!err) {
     console.log("Database is connected ... nn");
-} else {
+  } else {
     console.log("Error connecting database ... nn");
-}
+  }
 });
 
-router.get('/users', function(req, res, next){
-  connection.query('select * from users', function (err, rows, fields){
-    if (err){
-      console.log ('error');
-      return
-    } else{
+router.get("/users", function(req, res, next) {
+  connection.query("select * from users", function(err, rows, fields) {
+    if (err) {
+      console.log("error");
+      return;
+    } else {
       res.send(JSON.stringify(rows));
     }
   });
 });
 
+router.post("/book", function(req, res) {
+  var flightNo = req.body.flightNo;
+  connection.query(
+    "INSERT INTO records (flightNo, origin, destination, date_time, cost) SELECT flightNo, origin, destination, date_time, cost FROM flights WHERE flightNo = ?",
+    [flightNo],
+    function(error, results, fields) {
+      if (error) {
+        res.send({
+          code: 400,
+          failed: "error ocurred"
+        });
+      } else {
+        console.log("The solution is: ", results);
+        res.send({
+          code: 200,
+          success: "Booking successful"
+        });
+      }
+    }
+  );
+});
 
 //Bookings
-router.get('/bookings', function(req, res, next) {
-    connection.query('select * from bookings', function (error, results, fields) {
-        if(error) throw error;
-        res.send(JSON.stringify(results));
-    });
-});
-
-router.post('/addBookings', function(req, res){
- var bookings={
-   "bookingID":req.body.bookingID,
-   "firstName":req.body.firstName,
-   "lastName":req.body.lastName,
-   "email":req.body.email,
-   "payment":req.body.payment
- }
- connection.query('INSERT INTO bookings SET ?',bookings, function (error, results, fields) {
- if (error) {
-   console.log("error ocurred",error);
-   res.send({
-     "code":400,
-     "failed":"error ocurred"
-   })
- }else{
-   console.log('The solution is: ', results);
-   res.send({
-     "code":200,
-     "success":"Booking successful"
-       });
- }
- });
-});
-
- router.post('/register', function(req, res){
-  var today = new Date();
-  var users={
-    "firstName":req.body.firstName,
-    "lastName":req.body.lastName,
-    "email":req.body.email,
-    "password":req.body.password,
-    "created":today,
-    "modified":today
-  }
-  connection.query('INSERT INTO users SET ?',users, function (error, results, fields) {
-  if (error) {
-    console.log("error ocurred",error);
-    res.send({
-      "code":400,
-      "failed":"error ocurred"
-    })
-  }else{
-    console.log('The solution is: ', results);
-    res.send({
-      "code":200,
-      "success":"user registered sucessfully"
-        });
-  }
-  });
-});
-
-router.post('/auth', function(req, res) {
-  var email= req.body.email;
-  var password = req.body.password;
-  connection.query('SELECT * FROM users WHERE email = ?',[email], function (error, results, fields) {
-  if (error) {
-    res.send({
-      "code":400,
-      "failed":"error ocurred"
-    })
-  }else{
-    if(results.length >0){
-      if(results[0].password == password){
-        res.send({
-          "code":200,
-          "success":"Signin sucessfull"
-            });
-//            res.redirect('/users');
-      }
-      else{
-        res.send({
-          "code":204,
-          "error":"Email and password does not match"
-            });
-      }
-    }
-    else{
-      res.send({
-        "code":204,
-        "error":"Email does not exits"
-          });
-    }
-  }
-  });
-});
-
-router.get('/flights', function(req, res, next){
-  res.locals.connection.query('select * from flights', function(error, results, fields)
-  {
-    if(error)
-    throw error;
+router.get("/bookings", function(req, res, next) {
+  connection.query("select * from bookings", function(error, results, fields) {
+    if (error) throw error;
     res.send(JSON.stringify(results));
   });
 });
 
-router.get('/flightstwo', function(req, res, next){
-  connection.query('SELECT * FROM flightstwo', function(error, rows, fields)
-  {
-    if(error)
-    throw error;
-    res.send(JSON.stringify(rows));
+router.post("/addBookings", function(req, res) {
+  var bookings = {
+    bookingID: req.body.bookingID,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    payment: req.body.payment
+  };
+  connection.query("INSERT INTO bookings SET ?", bookings, function(
+    error,
+    results,
+    fields
+  ) {
+    if (error) {
+      console.log("error ocurred", error);
+      res.send({
+        code: 400,
+        failed: "error ocurred"
+      });
+    } else {
+      console.log("The solution is: ", results);
+      res.send({
+        code: 200,
+        success: "Booking successful"
+      });
+    }
   });
 });
 
+router.post("/register", function(req, res) {
+  var today = new Date();
+  var users = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    password: req.body.password,
+    created: today,
+    modified: today
+  };
+  connection.query("INSERT INTO users SET ?", users, function(
+    error,
+    results,
+    fields
+  ) {
+    if (error) {
+      console.log("error ocurred", error);
+      res.send({
+        code: 400,
+        failed: "error ocurred"
+      });
+    } else {
+      console.log("The solution is: ", results);
+      res.send({
+        code: 200,
+        success: "user registered sucessfully"
+      });
+    }
+  });
+});
 
+router.post("/auth", function(req, res) {
+  var email = req.body.email;
+  var password = req.body.password;
+  connection.query("SELECT * FROM users WHERE email = ?", [email], function(
+    error,
+    results,
+    fields
+  ) {
+    if (error) {
+      res.send({
+        code: 400,
+        failed: "error ocurred"
+      });
+    } else {
+      if (results.length > 0) {
+        if (results[0].password == password) {
+          res.send({
+            code: 200,
+            success: "Signin sucessfull"
+          });
+          //            res.redirect('/users');
+        } else {
+          res.send({
+            code: 204,
+            error: "Email and password does not match"
+          });
+        }
+      } else {
+        res.send({
+          code: 204,
+          error: "Email does not exits"
+        });
+      }
+    }
+  });
+});
 
+router.get("/flights", (req, res) => {
+  connection.query("SELECT * FROM flights", function(err, rows, fields) {
+    if (!err) {
+      console.log("Success getting flights");
+      res.send(JSON.stringify(rows));
+    } else {
+      console.log("Error getting flights");
+    }
+  });
+});
+
+router.post("/search", function(req, res) {
+  var origin = req.body.origin;
+  var destination = req.body.destination;
+  connection.query(
+    "SELECT * FROM flights WHERE origin = ? AND destination = ?",
+    [origin, destination],
+    function(error, results, fields) {
+      if (error) {
+        res.send({
+          code: 400,
+          failed: "error ocurred"
+        });
+      } else {
+        if (results.length > 0) {
+          console.log("Success getting flights");
+          res.send(JSON.stringify(results));
+        } else {
+          res.send({
+            code: 204,
+            success: "No flights exist"
+          });
+        }
+      }
+      console.log(results);
+    }
+  );
+});
 
 module.exports = router;
